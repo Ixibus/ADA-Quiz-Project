8fd939b552d1648fba4ee746af3f82dfaad4ec38
Franck CORCHER <corcher.franck@gmail.com>
2025-11-03 12:12:23 +0100

    saving .gitignore and src/

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,5 +1,9 @@
-console.log("hello");
-console.log("test");
-console.log("test");
-console.log("test3");
-console.log("test20");
+let array = [{question: "Comment je m'appelle?", proposition1: "Franck", proposition2: "Anna", proposition3: "Lisa", proposition4: "Pierre", réponse: this?.proposition1}];
+
+console.log(array[0]?.réponse);
+
+let question = document.querySelector(".question_container") as HTMLElement;
+
+console.log(question);
+
+question.addEventListener("click", () => console.log("yes"));
